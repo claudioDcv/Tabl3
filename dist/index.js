5d21f8440a1bd9c36cb70821d890d092dbddbee8
Claudio Rojas Rodriguez <clauidio.dcv@gmail.com>
2017-10-19 17:30:04 -0300

    v1.1.30

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _CompTabl = require('./CompTabl3');
+
+var _CompTabl2 = _interopRequireDefault(_CompTabl);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _CompTabl2.default;
+module.exports = exports['default'];
